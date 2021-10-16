a4d391b8ea01b7db403b4307798855573c9d4b9a
Jacob Kent <jacobgkent@gmail.com>
2021-10-16 13:15:12 -0400

    Made Vector 3D template class V1

--- OOP3200-F2020-Lesson3c/Vector3D.h
+++ OOP3200-F2020-Lesson3c/Vector3D.h
@@ -3,32 +3,30 @@
 #define __VECTOR3D__
 #include <string>
 
+template <class T>
 class Vector3D
 {
 public:
-	Vector3D(const float x = 0.0f, const float y = 0.0f, const float z = 0.0f);
+	Vector3D(const T x = static_cast<T>(0.0), const T y = static_cast < T>(0.0), const T z = static_cast < T>(0.0f));
 	~Vector3D();
 
 	// Accessors
-	float GetX() const;
-	float GetY() const;
-	float GetZ() const;
+	T GetX() const;
+	T GetY() const;
+	T GetZ() const;
 
 	// Mutators
-	void setX(const float x);
-	void setY(const float y);
-	void setZ(const float z);
-	void set(const float x, const float y, const float z);
-	
+	void setX(const T x);
+	void setY(const T y);
+	void setZ(const T z);
+	void set(const T x, const T y, const T z);
 
 	std::string ToString() const;
 
-
-
 private:
-	float m_x;
-	float m_y;
-	float m_z;
+	T m_x;
+	T m_y;
+	T m_z;
 };
 
 #endif/* defined (__VECTOR3D__) */
